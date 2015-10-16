c4a06ba5886eda6a8fc60d2dd6b5eae5153c153e
mayankraval <mayank.raval8293@gmail.com>
2015-10-16 14:04:55 -0400

    phase 2

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Hola, Mayank Raval !!!' , message : 'Welcome to my personal Web !!!'});
 });
 
+//Get about page
+router.get('/about', function(req,res,next) {
+    var ranNum = Math.random();
+    res.render('About',{title : 'About Me'});
+});
+
+//Get sevice page
+router.get('/services', function(req,res,next) {
+    var ranNum = Math.random();
+    res.render('Services',{title : 'Services'});
+});
+
 //Get contact page
 router.get('/contact', function(req,res,next) {
     var ranNum = Math.random();
-    res.render('contact',{title : 'Get in Touch!!!'});
+    res.render('Contact',{title : 'Get in Touch!!!'});
 });
 
 module.exports = router;
